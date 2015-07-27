8cb08282675c9a21215821927c5ecb070342081b
Oleg Svyryd <1996Oleg@bigmir.net>
2015-07-27 12:11:07 +0300

    Add typeHandler

--- AnimalWebApp/src/main/java/com/animals/app/domain/UserRole.java
+++ AnimalWebApp/src/main/java/com/animals/app/domain/UserRole.java
@@ -1,11 +1,19 @@
 package com.animals.app.domain;
 
+import org.apache.ibatis.type.JdbcType;
+import org.apache.ibatis.type.TypeHandler;
+
 import java.io.Serializable;
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by oleg on 22.07.2015.
  */
-public class UserRole implements Serializable{
+public class UserRole/* extends BaseTypeHandler<List<? extends UserRole>>*/ implements Serializable, TypeHandler<List<? extends UserRole>> {
 
     private Integer id;
     private String role;
@@ -56,4 +64,46 @@ public class UserRole implements Serializable{
                 ", id=" + id +
                 '}';
     }
+
+    @Override
+    public void setParameter(PreparedStatement preparedStatement, int i, List<? extends UserRole> userRoles, JdbcType jdbcType) throws SQLException {
+        preparedStatement.setObject(i, userRoles.get(0).getId());
+    }
+
+    @Override
+    public List<? extends UserRole> getResult(ResultSet resultSet, String s) throws SQLException {
+        return (List<? extends UserRole>) resultSet.getObject(s);
+    }
+
+    @Override
+    public List<? extends UserRole> getResult(ResultSet resultSet, int i) throws SQLException {
+        return (List<? extends UserRole>) resultSet.getObject(i);
+    }
+
+    @Override
+    public List<? extends UserRole> getResult(CallableStatement callableStatement, int i) throws SQLException {
+        return null;
+    }
+
+/*
+    @Override
+    public void setNonNullParameter(PreparedStatement preparedStatement, int i, List<? extends UserRole> list, JdbcType jdbcType) throws SQLException {
+        preparedStatement.setObject(i, list.get(0).getId());
+    }
+
+    @Override
+    public List getNullableResult(ResultSet resultSet, String s) throws SQLException {
+        return (List) resultSet.getObject(s);
+    }
+
+    @Override
+    public List getNullableResult(ResultSet resultSet, int i) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public List getNullableResult(CallableStatement callableStatement, int i) throws SQLException {
+        return null;
+    }
+*/
 }
